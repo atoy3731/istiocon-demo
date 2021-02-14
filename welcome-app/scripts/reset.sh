2e4e5cefe4ea8a1dad24acc71747d57a73f60680
Adam Toy <adam.toy@rancherfederal.com>
2021-02-14 11:29:13 -0500

    Update

---
+++ welcome-app/scripts/reset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Deleting backend authpolicy.."
+kubectl delete authorizationpolicy -n welcome-app backend-authconfig
+
+echo "Deleting application to reset it.."
+kubectl delete -f ../base-manifests
+
+echo "Confirming namespace was terminated.."
+while [ ! -n "$(kubectl get namespace welcome-app 2>&1 > /dev/null)" ]; do
+    sleep 1
+done
+
+echo "Creating namespace.."
+kubectl apply -f ../base-manifests/namespace.yaml
+sleep 3
+
+echo "Creating application resources.."
+kubectl apply -f ../base-manifests
